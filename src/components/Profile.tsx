e0247f3bdb71abb3439dcaef1a4ee5c279831ca3
tiago-silva-analista <tiago.silva.analista@gmail.com>
2021-02-25 18:47:09 -0300

    Commite initial com o conteúdo da programação do primeiro e segundo dia

---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/tiago-silva-analista.png" alt="Tiago Santos" />
+            <div>
+                <strong>
+                    Tiago Santos
+                </strong>
+                <p>
+                    <img src="icons/level.svg" alt="" />
+                    Level 1
+                </p>
+            </div>
+        </div>
+    )
+}
